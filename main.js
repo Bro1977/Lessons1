c180feb504f3824aa582c4ea622706445b8b3fc5
Bro1977 <ajymuratburkanov@gmail.com>
2023-06-20 20:30:13 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+let num = 10;
+let str ='Hello world';
+let bool = true;
+let nul = null;
+let undef = undefined;
+let obj = {};
+let num2 = "10";
+
+//sumbol
+//bigInt
+// console.log(10 + '1');
+//Создайте постаяннную которая содержит ваше имя
+//Создайте 
+//
+
+const name = 'Ajymurat';
+let age = 18;
+
+// if(num > 9){
+    // console.log('Hello World!');
+// } else if(num === 5){
+    // console.log('Hello Geeks!');
+// } else{
+    // console.log('bye');
+// }
+
+if(num % 2 === 0){
+    console.log('Четное число')
+} else{
+    console.log('Нечетное число')
+}
+
+alert('Hello world')
